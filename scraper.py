283f740d45fd4f0258ddd441dfab660d64637e42
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-02-03 18:52:21 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -267,7 +267,8 @@ while jsonprods:
                             # >>> VISIT THE PAGE THROUGH BROWSER <<< #
                             try:
                                 browser.visit(product['url'])
-                                #time.sleep(2)
+                                time.sleep(2)
+                                browser.driver.refresh()
                                 html_source = browser.html
                                 #html_source = browser.driver.page_source
                                 #html_source = browser.driver.execute_script('return document.documentElement.outerHTML')
