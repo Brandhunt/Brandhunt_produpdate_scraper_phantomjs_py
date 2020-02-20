f45467ecb814819b0105ef879d0e95cf0d865a09
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-02-21 00:34:32 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -46,10 +46,11 @@ with open(__file__, 'r') as file:
         file_text = '' + ''.join(file.readlines()) + os.environ['MORPH_WONPR_SECRET_KEY'] + ''
         #print(file_text)
         encodedfile = base64.b64encode(file_text.encode())
-        print(encodedfile)
+        #print(encodedfile)
         scraperwiki.sqlite.save(unique_keys=['fileid'],\
                                 data={'fileid': 0, 'file': json.dumps(str(encodedfile))},\
                                 table_name = 'filestoexport')
+        print(json.dumps(str(encodedfile)))
         print('Main file export successful!')
     except:
         print(traceback.format_exc())
