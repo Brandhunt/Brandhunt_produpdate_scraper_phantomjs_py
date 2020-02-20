d6dca116ad968728bf693338e53ca9dca32548b7
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-02-21 00:53:02 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -47,8 +47,11 @@ with open(__file__, 'r') as file:
         #print(file_text)
         encodedfile = base64.b64encode(file_text.encode())
         #print(encodedfile)
+        #scraperwiki.sqlite.save(unique_keys=['fileid'],\
+        #                        data={'fileid': 0, 'file': json.dumps(str(encodedfile))},\
+        #                        table_name = 'filestoexport')
         scraperwiki.sqlite.save(unique_keys=['fileid'],\
-                                data={'fileid': 0, 'file': json.dumps(str(encodedfile))},\
+                                data={'fileid': 0, 'file': 'HEPP'},\
                                 table_name = 'filestoexport')
         #print(json.dumps(str(encodedfile)))
         print('Main file export successful!')
