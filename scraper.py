d4d3f6e8fe0bc9e7115d56f30e9c8b4ecd405050
Brandhunt <57029610+Brandhunt@users.noreply.github.com>
2020-06-17 13:08:40 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -692,7 +692,7 @@ def mainfunc(maxlimit):
                                                 for size_handle_arr in size_handle_arrs:
                                                     size_handle_arr = size_handle_arr.strip().split(':')
                                                     if len(size_handle_arr) < 3:
-                                                        size_handling_options.append([ int(size_handle_arr[0]), size_handle_arr[1], '' ])
+                                                        size_handling_options.append([ int(size_handle_arr[0]), size_handle_arr[1], '/' ])
                                                     else:
                                                         size_handling_options.append([ int(size_handle_arr[0]), size_handle_arr[1], size_handle_arr[2] ])
                                                 productmisc_array[i] = 'true'
